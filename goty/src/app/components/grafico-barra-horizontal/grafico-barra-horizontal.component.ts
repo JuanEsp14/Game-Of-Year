315561a5cab3f5c21e92b51ebe48ab619ddcc22a
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-02 15:54:50 -0300

    Se agrega la gráfica horizontal a través de NGX y CDK

--- goty/src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ goty/src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
@@ -7,7 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraficoBarraHorizontalComponent implements OnInit {
 
-  constructor() { }
+  results: any[] = [{
+    "name": "Game 1",
+    "value": 500
+  },
+  {
+    "name": "Game 2",
+    "value": 300
+  },
+  {
+    "name": "Game 3",
+    "value": 400
+  },
+  {
+    "name": "Game 4",
+    "value": 200
+  }];
+
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Games';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votes';
+
+  colorScheme = 'nightLights';
+
+  constructor() {
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
 
   ngOnInit() {
   }
