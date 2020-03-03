6ca8845e740355149c0c8cc9fffdd77c1aec6ab9
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-03 09:29:26 -0300

    Se añade el registro de autorización a la base de datos

--- goty-backend/functions/src/index.ts
+++ goty-backend/functions/src/index.ts
@@ -1,4 +1,12 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://goty-78579.firebaseio.com"
+});
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
