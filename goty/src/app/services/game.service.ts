5c10a80dd9934ca54ea4c513ae93a4aefc4de5d0
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-03 11:15:45 -0300

    Se agrega una variable en el servicio para no consumir desde Firebase cuando ya se encuentra la información

--- goty/src/app/services/game.service.ts
+++ goty/src/app/services/game.service.ts
@@ -3,14 +3,29 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
 
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
 
+  private games: Game[] = [];
+
   constructor( private http: HttpClient ) { }
 
   getNominados(){
-    return this.http.get<Game[]>(`${ environment.url }/api/goty`);
+
+    if( this.games.length === 0){
+      console.log('Desde internet')
+      return this.http.get<Game[]>(`${ environment.url }/api/goty`)
+              .pipe(
+                tap(games => this.games = games)
+              );
+    }
+    console.log('Desde caché');
+    return of(this.games);
+    
   }
 }
