8e4b5414814f32bcc9e7f6235b5192409db793f7
jespinoza <juan.espinoza@fluxit.com.ar>
2020-03-03 10:01:46 -0300

    Se crea el servicio express de Node para trabajar con los pedidos

--- goty-backend/functions/src/index.ts
+++ goty-backend/functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express'
+import * as cors from 'cors';
 
 const serviceAccount = require("./serviceAccountKey.json");
 
@@ -31,3 +33,18 @@ export const getGOTY = functions.https.onRequest(async(request, response) => {
   response.json( juegos );
 });
 
+/**Servidor Express */
+const app = express();
+app.use( cors({ origin: true }) );
+
+//Esta función es la misma que getGOTY pero creada desde un servidor de NODE
+app.get('/goty',async ( req, resp) => {
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const juegos = docsSnap.docs.map( doc => doc.data());
+
+  resp.json( juegos );
+});
+
+//Se le indica a firebase que hay un servidor express corriendo
+export const api = functions.https.onRequest( app );
